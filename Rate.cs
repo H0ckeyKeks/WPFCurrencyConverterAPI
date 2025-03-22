b9f5397c4259344a56699bd774ee3f691a9f7cd0
Jasmin <jasmin_suedhoff@web.de>
2025-03-22 16:01:27 +0100

    Projektdateien hinzufügen.

---
+++ Rate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCurrencyConverterAPI
+{
+    public class Rate
+    {
+        // Caution: It is neccesray to use exactly the same names as the API Return Value Names
+        public double INR { get; set; }     // Indian Rupee
+        public double JPY { get; set; }     // Japanese Yen
+        public double USD { get; set; }     // US Dollar
+        public double NZD { get; set; }     // New Zealand Dollar 
+        public double EUR { get; set; }     // Euro
+        public double CAD { get; set; }     // Canadian Dollar
+        public double ISK { get; set; }     // Icelandic Króna
+        public double PHP { get; set; }     // Philippine Peso
+        public double DKK { get; set; }     // Danish Kroner
+        public double CZK { get; set; }     // Czech Koruna
+    }
+}
---
+++ Root.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCurrencyConverterAPI
+{
+    // Root class is a Main class. API Return rates in a class rates returns all currency names with value
+    public class Root
+    {
+        // Getting all Record in rates and set in rate class as currency name wise
+        public Rate rates { get; set; }  // Needs to have the same name as the Return Value of the API
+       
+        public long timestamp;
+
+        public string license;
+
+    }
+}
